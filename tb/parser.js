732994a1ab15eb219b245f02fb22a52453b24385
Karen Coombs <coombsk@oclc.org>
2020-09-29 20:19:51 -0500

    Fixed parser code formatting

--- tb/parser.js
+++ tb/parser.js
@@ -21,59 +21,70 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
 
   let match;
 
-  if ((match = /^\/Default.aspx$/i.exec(path)) !== null) {
-    // https://www.tumblebooklibrary.com/Default.aspx?ReturnUrl=/TumbleSearch.aspx
-    result.rtype    = 'OTHER';
+  if ((match = /^\/([a-z]+).aspx$/i.exec(path)) !== null) {
     result.mime     = 'HTML';
-
-    /**
-     * unitid is a crucial information needed to filter double-clicks phenomenon, like described by COUNTER
-     * it described the most fine-grained of what's being accessed by the user
-     * it can be a DOI, an internal identifier or a part of the accessed URL
-     * more at http://ezpaarse.readthedocs.io/en/master/essential/ec-attributes.html#unitid
-     */
-    result.unitid = 'none';
-
-  } else if ((match = /^\/Home.aspx$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.html?sequence=1
-    result.rtype    = 'OTHER';
-    result.mime     = 'HTML';
-    result.unitid = 'none';
-  } else if ((match = /^\/BooksList.aspx$/i.exec(path)) !== null) {
-    // https://www.tumblebooklibrary.com/BooksList.aspx?categoryID=77
-    result.rtype    = 'OTHER';
-    result.mime     = 'HTML';
-    result.unitid = 'none';
-  } else if ((match = /^\/TumbleSearch.aspx$/i.exec(path)) !== null) {
-    // https://www.tumblebooklibrary.com/TumbleSearch.aspx
-    result.rtype    = 'SEARCH';
-    result.mime     = 'HTML';
-    result.unitid = 'none';
-  } else if ((match = /^\/Result.aspx$/i.exec(path)) !== null) {
-    // https://www.tumblebooklibrary.com/Result.aspx?m=Title&key=pie
-    result.rtype    = 'SEARCH';
-    result.mime     = 'HTML';
-    result.unitid = 'none';
-  } else if ((match = /^\/book.aspx$/i.exec(path)) !== null) {
-    // https://www.tumblebooklibrary.com/book.aspx?id=4229
-    result.rtype    = 'RECORD';
-    result.mime     = 'HTML';
-    result.title_id = param.id;
-    result.unitid = param.id;
-  } else if ((match = /^\/Video.aspx$/i.exec(path)) !== null) {
-    // https://www.tumblebooklibrary.com/Video.aspx?ProductID=4229
-    result.rtype    = 'BOOK';
-    result.mime     = 'HTML';
-    result.title_id = param.ProductID;
-    result.unitid = param.ProductID;
+    const pageName = match[1].toLowerCase();
+    switch (pageName) {
+    case 'default': {
+      // https://www.tumblebooklibrary.com/Default.aspx?ReturnUrl=/TumbleSearch.aspx
+      result.unitid = 'none';
+      result.rtype ='OTHER';
+      break;
+    }
+    case 'home': {
+      result.rtype    = 'OTHER';
+      result.unitid = 'none';
+      break;
+    }
+    case 'bookslist': {
+      // https://www.tumblebooklibrary.com/BooksList.aspx?categoryID=77
+      result.rtype    = 'OTHER';
+      result.unitid = 'none';
+      break;
+    }
+    case 'tumblesearch': {
+      // https://www.tumblebooklibrary.com/TumbleSearch.aspx
+      result.rtype    = 'SEARCH';
+      result.unitid = 'none';
+      break;
+    }
+    case 'result': {
+      // https://www.tumblebooklibrary.com/Result.aspx?m=Title&key=pie
+      result.rtype    = 'SEARCH';
+      result.unitid = 'none';
+      break;
+    }
+    case 'book': {
+      // https://www.tumblebooklibrary.com/book.aspx?id=4229
+      result.rtype    = 'RECORD';
+      result.title_id = param.id;
+      result.unitid = param.id;
+      break;
+    }
+    case 'video': {
+      // https://www.tumblebooklibrary.com/Video.aspx?ProductID=4229
+      result.rtype    = 'BOOK';
+      result.title_id = param.ProductID;
+      result.unitid = param.ProductID;
+      break;
+    }
+    case 'viewonline': {
+      // https://www.tumblebooklibrary.com/ViewOnline.aspx?Is5=true&ProductID=6219
+      result.rtype    = 'BOOK';
+      result.title_id = param.ProductID;
+      result.unitid = param.ProductID;
+      break;
+    }
+    case 'raplayer': {
+      // https://www.tumblebooklibrary.com/RAPlayer.aspx?ProductID=5791
+      result.rtype    = 'BOOK';
+      result.title_id = param.ProductID;
+      result.unitid = param.ProductID;
+      break;
+    }
+    }
   } else if ((match = /^\/H5Player.aspx\/$/i.exec(path)) !== null) {
     // https://www.tumblebooklibrary.com/H5Player.aspx/?ProductID=7075&book=%2FH5Books%2FTBLEN%2Fbooks%2Fbabyanimalsplaying%2Fbabyanimalsplaying.json&page=0
-    result.rtype    = 'BOOK';
-    result.mime     = 'HTML';
-    result.title_id = param.ProductID;
-    result.unitid = param.ProductID;
-  } else if ((match = /^\/ViewOnline.aspx$/i.exec(path)) !== null) {
-    // https://www.tumblebooklibrary.com/ViewOnline.aspx?Is5=true&ProductID=6219
     result.rtype    = 'BOOK';
     result.mime     = 'HTML';
     result.title_id = param.ProductID;
@@ -84,14 +95,6 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.mime     = 'HTML';
     result.title_id = param.ProductID;
     result.unitid = param.ProductID;
-  } else if ((match = /^\/RAPlayer.aspx$/i.exec(path)) !== null) {
-    // https://www.tumblebooklibrary.com/RAPlayer.aspx?ProductID=5791
-    result.rtype    = 'BOOK';
-    result.mime     = 'HTML';
-    result.title_id = param.ProductID;
-    result.unitid = param.ProductID;
   }
-  
-
   return result;
 });
